060b091764f3bbdb986cdeeafd5e8cbae45ec672
Viktor Qorri <viktor.j.l.s.qorri@gmail.com>
2026-07-22 16:29:07 +0300

    Revert "V2.0"

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
 #Imports
 import random as rd
-import matplotlib.pyplot as plt
 
 #Variables
 cycles = 10000000 #set this number for the precision
-plotcycles = 10000 #max 10.000 is recomended, gets calculated automaticaly if cycles < 10.000
 inside = 0
 estimation = 0
-xpoints = []
-ypoints = []
-pointcolors = []
-
-if cycles < plotcycles:
-    plotcycles = cycles
 
 #Functions
 def calcInside(x,y):
     global cycles
     global inside
     if pow(x,2) + pow(y,2) <= 1:
-        return True
+        inside += 1
     else:
-        return False
+        return
 
 def calcPi():
     global inside
@@ -31,25 +23,10 @@ def calcPi():
 
 
 #Main
-for cycle in range(plotcycles):
+for cycle in range(cycles):
     x = rd.random()
     y = rd.random()
-    xpoints.append(x)
-    ypoints.append(y)
-    if calcInside(x,y):
-        inside += 1
-        pointcolors.append("green")
-    else:
-        pointcolors.append("red")
-
-for cycle in range(cycles - plotcycles):
-    x = rd.random()
-    y = rd.random()
-    if calcInside(x,y):
-        inside += 1
+    calcInside(x,y)
 
 calcPi()
 print(estimation)
-plt.gca().set_aspect('equal') #Plot aspect ratio set to "equal" (1:1 aspect)
-plt.scatter(xpoints,ypoints,c=pointcolors)
-plt.show()
